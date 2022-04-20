b5c6a09c2c70431ccac08e4e0ccebd102719219e
rotterbein <49872554+rotterbein@users.noreply.github.com>
2022-04-20 17:28:01 -0400

    Setup mc processing for filter magnitudes

--- source/projects/ddsp.decoder_controls_tilde/ddsp.decoder_controls_tilde.cpp
+++ source/projects/ddsp.decoder_controls_tilde/ddsp.decoder_controls_tilde.cpp
@@ -16,16 +16,18 @@
 
 #define B_SIZE 256
 #define N_HARMONICS 100
+#define N_MAGNITUDES 65
 
 using namespace c74::min;
 namespace max = c74::max;
+//using namespace std::chrono;
 
 class DDSPModel
 {
 public:
     DDSPModel();
     int load(std::string path);
-    void perform(double *pitch, double *loudness, double *f0, double *harmonic_amplitudes, int buffer_size);
+    void perform(double *pitch, double *loudness, double *f0, double *harmonic_amplitudes, double *filter_magnitudes, int buffer_size);
 
 private:
     torch::jit::script::Module m_scripted_model;
@@ -54,7 +56,7 @@ int DDSPModel::load(std::string path)
     }
 }
 
-void DDSPModel::perform(double *pitch, double *loudness, double *f0, double *harmonic_amplitudes, int buffer_size)
+void DDSPModel::perform(double *pitch, double *loudness, double *f0, double *harmonic_amplitudes, double *filter_magnitudes, int buffer_size)
 {
     torch::NoGradGuard no_grad;
     if (m_model_is_loaded)
@@ -64,35 +66,49 @@ void DDSPModel::perform(double *pitch, double *loudness, double *f0, double *har
         auto loudness_tensor = torch::from_blob(loudness, {1, buffer_size, 1}, options);
 
         std::vector<torch::jit::IValue> inputs = {pitch_tensor.to(torch::kFloat32), loudness_tensor.to(torch::kFloat32)};
-
+        
+//        auto start_time = high_resolution_clock::now();
         auto out_tensor = m_scripted_model.forward(inputs);
+//        auto stop_time = high_resolution_clock::now();
+//        auto duration = duration_cast<microseconds>(stop_time - start_time);
+//        std::cout << duration.count() << std::endl;
         auto harmonic_amplitudes_tensor = out_tensor.toTuple()->elements()[0].toTensor().to(torch::kFloat64);
         auto filter_magnitudes_tensor = out_tensor.toTuple()->elements()[1].toTensor().to(torch::kFloat64);
 
         // upsampling / interpolation
         namespace F = torch::nn::functional;
-        harmonic_amplitudes_tensor = harmonic_amplitudes_tensor.contiguous();
-        harmonic_amplitudes_tensor = harmonic_amplitudes_tensor.permute({0, 2, 1}); // permute: switch rows and columns, rows = harmonics, columns = samples
-        harmonic_amplitudes_tensor = F::interpolate(harmonic_amplitudes_tensor, F::InterpolateFuncOptions()
-                                                    .size(std::vector<int64_t>{harmonic_amplitudes_tensor.size(2) * buffer_size})
-                                                    .mode(torch::kNearest));
-//        harmonic_amplitudes_tensor = harmonic_amplitudes_tensor.permute({0, 2, 1});
-        harmonic_amplitudes_tensor = torch::flatten(harmonic_amplitudes_tensor, 1);
-        
         pitch_tensor = pitch_tensor.contiguous();
         pitch_tensor = pitch_tensor.permute({0, 2, 1});
         pitch_tensor = F::interpolate(pitch_tensor, F::InterpolateFuncOptions()
                                                     .size(std::vector<int64_t>{pitch_tensor.size(2) * buffer_size})
-                                                    .mode(torch::kNearest));
+                                                    .mode(torch::kNearest)); // originally DDSP uses linear method
         pitch_tensor = pitch_tensor.permute({0, 2, 1});
         pitch_tensor = torch::flatten(pitch_tensor);
         
+        harmonic_amplitudes_tensor = harmonic_amplitudes_tensor.contiguous();
+        harmonic_amplitudes_tensor = harmonic_amplitudes_tensor.permute({0, 2, 1}); // permute: switch rows and columns, rows = harmonics, columns = samples
+        harmonic_amplitudes_tensor = F::interpolate(harmonic_amplitudes_tensor, F::InterpolateFuncOptions()
+                                                    .size(std::vector<int64_t>{harmonic_amplitudes_tensor.size(2) * buffer_size})
+                                                    .mode(torch::kNearest)); // originally DDSP uses window method
+//        harmonic_amplitudes_tensor = harmonic_amplitudes_tensor.permute({0, 2, 1});
+        harmonic_amplitudes_tensor = torch::flatten(harmonic_amplitudes_tensor, 1);
+        
+        filter_magnitudes_tensor = filter_magnitudes_tensor.contiguous();
+        filter_magnitudes_tensor = filter_magnitudes_tensor.permute({0, 2, 1});
+        filter_magnitudes_tensor = F::interpolate(filter_magnitudes_tensor, F::InterpolateFuncOptions()
+                                                    .size(std::vector<int64_t>{filter_magnitudes_tensor.size(2) * buffer_size})
+                                                    .mode(torch::kNearest));
+        filter_magnitudes_tensor = torch::flatten(filter_magnitudes_tensor, 1);
+        
         // Copy to output buffers
+        auto pitch_ptr = pitch_tensor.contiguous().data_ptr<double>();
+        memcpy(f0, pitch_ptr, buffer_size * sizeof(double));
+        
         auto harmonic_amplitudes_ptr = harmonic_amplitudes_tensor.contiguous().data_ptr<double>();
         memcpy(harmonic_amplitudes, harmonic_amplitudes_ptr, buffer_size * N_HARMONICS * sizeof(double));
         
-        auto pitch_ptr = pitch_tensor.contiguous().data_ptr<double>();
-        memcpy(f0, pitch_ptr, buffer_size * sizeof(double));
+        auto filter_magnitudes_ptr = filter_magnitudes_tensor.contiguous().data_ptr<double>();
+        memcpy(filter_magnitudes, filter_magnitudes_ptr, buffer_size * N_MAGNITUDES * sizeof(double));
     }
 }
 
@@ -104,9 +120,9 @@ public:
     MIN_RELATED		{ "" };
 
     // execute the ddsp computation in a separate thread
-    void thread_perform(double *pitch, double *loudness, double *f0, double *harmonic_amplitudes, int buffer_size)
+    void thread_perform(double *pitch, double *loudness, double *f0, double *harmonic_amplitudes, double *filter_magnitudes, int buffer_size)
     {
-        m_model->perform(pitch, loudness, f0, harmonic_amplitudes, buffer_size);
+        m_model->perform(pitch, loudness, f0, harmonic_amplitudes, filter_magnitudes, buffer_size);
     }
 
     // constructor
@@ -153,16 +169,26 @@ public:
         memcpy(m_loudness_buffer + m_head, input.samples(1), n * sizeof(double));
         memcpy(output.samples(0), m_f0_buffer + m_head, output.frame_count() * sizeof(double));
         
-        int offset = m_head % B_SIZE;
+        // Calculate offset for mc buffers
+        int offset_harmonics = m_head % B_SIZE;
+        int offset_magnitudes = m_head % B_SIZE;
         if (m_head >= B_SIZE) { // point to next part of the buffer
-            offset += N_HARMONICS * B_SIZE;
+            offset_harmonics += N_HARMONICS * B_SIZE;
+            offset_magnitudes += N_MAGNITUDES * B_SIZE;
         }
         
         // Copy harmonics into according channels
         for (int harmonic = 0; harmonic < N_HARMONICS; harmonic++) {
             int ch = harmonic + 1;
-            memcpy(output.samples(ch), m_harmonic_amplitudes_buffer + offset, output.frame_count() * sizeof(double));
-            offset += B_SIZE;
+            memcpy(output.samples(ch), m_harmonic_amplitudes_buffer + offset_harmonics, output.frame_count() * sizeof(double));
+            offset_harmonics += B_SIZE;
+        }
+        
+        // Copy filter magnitudes into according channels
+        for (int magnitude = 0; magnitude < N_MAGNITUDES; magnitude++) {
+            int ch = magnitude + N_HARMONICS + 1;
+            memcpy(output.samples(ch), m_filter_magnitudes_buffer + offset_magnitudes, output.frame_count() * sizeof(double));
+            offset_magnitudes += B_SIZE;
         }
         
         m_head += n; // progress with the m_head
@@ -177,6 +203,7 @@ public:
                                             m_loudness_buffer + m_model_head,
                                             m_f0_buffer + m_model_head,
                                             m_harmonic_amplitudes_buffer + N_HARMONICS * m_model_head,
+                                            m_filter_magnitudes_buffer + N_MAGNITUDES * m_model_head,
                                             B_SIZE); // compute the buffers in a separate thread
 
             m_head = m_head % (2 * B_SIZE); // set the m_head to the next available value
@@ -219,7 +246,7 @@ private:
     double m_loudness_buffer[2 * B_SIZE];
     double m_f0_buffer[2 * B_SIZE];
     double m_harmonic_amplitudes_buffer[2 * B_SIZE * N_HARMONICS];
-    double m_filter_magnitudes_buffer[2 * B_SIZE][65];
+    double m_filter_magnitudes_buffer[2 * B_SIZE * N_MAGNITUDES];
 
     // variable to store the position in the buffer
     int m_head {0};
